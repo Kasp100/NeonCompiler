af69e48b13c887e8a86185106460fdf6603eae27
Kasper Declercq <kasper.declercq1@gmail.com>
2025-05-11 01:19:23 +0200

    Fix logger methods should be const in definition

--- logging/logger.cpp
+++ logging/logger.cpp
@@ -8,22 +8,22 @@ using namespace std;
 
 Logger::Logger() {}
 
-void Logger::error(const string& message)
+void Logger::error(const string& message) const
 {
     cerr << "\x1B[31mError\033[0m\t" << message << endl;
 }
 
-void Logger::warning(const string& message)
+void Logger::warning(const string& message) const
 {
     cerr << "\x1B[33mWarning\033[0m\t" << message << endl;
 }
 
-void Logger::info(const string& message)
+void Logger::info(const string& message) const
 {
     cerr << "\x1B[32mInfo\033[0m\t" << message << endl;
 }
 
-void Logger::debug(const string& message)
+void Logger::debug(const string& message) const
 {
     cerr << "\x1B[35mDebug\033[0m\t" << message << endl;
 }
