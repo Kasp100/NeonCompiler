805106c27406e2aa5956c226a67cf26cc19813f6
Kasper Declercq <kasper.declercq1@gmail.com>
2025-05-10 15:53:35 +0200

    Create Token

---
+++ neon_compiler/token.cpp
@@ -0,0 +1,47 @@
+#include "token.hpp"
+
+using namespace compiler;
+using namespace std;
+
+Token::Token(TokenType type, uint32_t row, uint32_t col, optional<string_view> lexeme)
+		: type(type), row(row), col(col), lexeme(lexeme) {}
+
+
+optional<TokenType> Token::keyword_to_token_type(string_view word)
+{
+	     if (word == "pkg")              return TokenType::PACKAGE;
+	else if (word == "public")           return TokenType::VISIBILITY_PUBLIC;
+	else if (word == "private")          return TokenType::VISIBILITY_PRIVATE;
+	else if (word == "protected")        return TokenType::VISIBILITY_PROTECTED;
+	else if (word == "inner")            return TokenType::VISIBILITY_INNER;
+	else if (word == "static")           return TokenType::MEMBER_STATIC;
+	else if (word == "const")            return TokenType::MEMBER_CONST;
+	else if (word == "external")         return TokenType::MEMBER_EXTERNAL;
+	else if (word == "mut:")             return TokenType::MUTABLE_REFERENCE;
+	else if (word == "mut")              return TokenType::MUTABLE_DECLARATION;
+	else if (word == "own")              return TokenType::REF_TYPE_OWN;
+	else if (word == "shared")           return TokenType::REF_TYPE_SHARED;
+	else if (word == "class")            return TokenType::TYPE_CLASS;
+	else if (word == "interface")        return TokenType::TYPE_INTERFACE;
+	else if (word == "constructor")      return TokenType::CONSTRUCTOR;
+	else if (word == "super")            return TokenType::REFERENCE_SUPER;
+	else if (word == "this")             return TokenType::REFERENCE_THIS;
+	else if (word == "impl")             return TokenType::INHERITANCE_IMPLEMENTS;
+	else if (word == "extends")          return TokenType::INHERITANCE_EXTENDS;
+	else if (word == "extendable")       return TokenType::DECL_CLASS_EXTENDABLE;
+	else if (word == "final")            return TokenType::DECL_METHOD_FINAL;
+	else if (word == "override")         return TokenType::DECL_METHOD_OVERRIDE;
+	else if (word == "ret")              return TokenType::STMT_RETURN;
+	else if (word == "move")             return TokenType::STMT_MOVE;
+	else if (word == "pass")             return TokenType::STMT_PASS;
+	else if (word == "copy")             return TokenType::STMT_COPY;
+	else if (word == "copyable")         return TokenType::DECL_TYPE_COPYABLE;
+	else if (word == "serialisable")     return TokenType::DECL_TYPE_SERIALISABLE;
+	else if (word == "true")             return TokenType::BOOL_TRUE;
+	else if (word == "false")            return TokenType::BOOL_FALSE;
+	else if (word == "auto:")            return TokenType::CALL_COMPILE_FUNCTION;
+	else if (word == "compile_function") return TokenType::DEFINE_COMPILE_FUNCTION;
+	else if (word == "grammar")          return TokenType::DEFINE_GRAMMAR_SET;
+
+	return nullopt;
+}
---
+++ neon_compiler/token.hpp
@@ -0,0 +1,77 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+#include <cstdint>
+#include <optional>
+#include <string_view>
+
+namespace compiler
+{
+
+enum class TokenType
+{
+	IDENTIFIER,
+	LITERAL_NUMBER,
+	LITERAL_STRING,
+	BRACKET_CURLY_OPEN,
+	BRACKET_CURLY_CLOSE,
+	BRACKET_ROUND_OPEN,
+	BRACKET_ROUND_CLOSE,
+	SMALLER_THAN,
+	GREATER_THAN,
+	PACKAGE,
+	STATIC_ACCESSOR,
+	VISIBILITY_PUBLIC,
+	VISIBILITY_PRIVATE,
+	VISIBILITY_PROTECTED,
+	VISIBILITY_INNER,
+	MEMBER_STATIC,
+	MEMBER_CONST,
+	MEMBER_EXTERNAL,
+	MUTABLE_REFERENCE,
+	MUTABLE_DECLARATION,
+	REF_TYPE_OWN,
+	REF_TYPE_SHARED,
+	TYPE_CLASS,
+	TYPE_INTERFACE,
+	CONSTRUCTOR,
+	REFERENCE_SUPER,
+	REFERENCE_THIS,
+	INHERITANCE_IMPLEMENTS,
+	INHERITANCE_EXTENDS,
+	DECL_CLASS_EXTENDABLE,
+	DECL_METHOD_FINAL,
+	DECL_METHOD_OVERRIDE,
+	STMT_RETURN,
+	STMT_MOVE,
+	STMT_PASS,
+	STMT_COPY,
+	DECL_TYPE_COPYABLE,
+	DECL_TYPE_SERIALISABLE,
+	BOOL_TRUE,
+	BOOL_FALSE,
+	CALL_COMPILE_FUNCTION,
+	DEFINE_COMPILE_FUNCTION,
+	DEFINE_GRAMMAR_SET
+};
+
+class Token {
+
+public:
+	explicit Token(TokenType type, std::uint32_t row, std::uint32_t col, std::optional<std::string_view> lexeme = std::nullopt);
+
+	TokenType get_type() const;
+	std::uint32_t get_row() const;
+	std::uint32_t get_col() const;
+	std::optional<std::string_view> get_lexeme() const;
+
+	static std::optional<TokenType> keyword_to_token_type(std::string_view word);
+
+private:
+	TokenType type;
+	std::uint32_t row, col;
+	std::optional<std::string_view> lexeme;
+};
+}
+
+#endif // TOKEN_HPP
