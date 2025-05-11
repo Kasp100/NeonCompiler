6d7ca64df26e521d1af2391aa65f6b6b8526bf93
Kasper Declercq <kasper.declercq1@gmail.com>
2025-05-11 15:04:49 +0200

    Change logging format

--- logging/logger.cpp
+++ logging/logger.cpp
@@ -8,22 +8,24 @@ using namespace std;
 
 Logger::Logger() {}
 
+constexpr const char* PREFIX_LOG = "[Log] ";
+
 void Logger::error(const string& message) const
 {
-    cerr << "\x1B[31mError\033[0m\t" << message << endl;
+    cerr << PREFIX_LOG << "\x1B[31mError\033[0m " << message << endl;
 }
 
 void Logger::warning(const string& message) const
 {
-    cerr << "\x1B[33mWarning\033[0m\t" << message << endl;
+    cerr << PREFIX_LOG << "\x1B[33mWarning\033[0m " << message << endl;
 }
 
 void Logger::info(const string& message) const
 {
-    cerr << "\x1B[32mInfo\033[0m\t" << message << endl;
+    cerr << PREFIX_LOG << "\x1B[32mInfo\033[0m " << message << endl;
 }
 
 void Logger::debug(const string& message) const
 {
-    cerr << "\x1B[35mDebug\033[0m\t" << message << endl;
+    cerr << PREFIX_LOG << "\x1B[35mDebug\033[0m " << message << endl;
 }
