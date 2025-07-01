832efc53b1e205c4227a50aa31afac16b2145876
Kasper Declercq <kasper.declercq1@gmail.com>
2025-07-01 18:33:46 +0200

    Add `std::` to `move()` calls

--- neon_compiler/compiler.cpp
+++ neon_compiler/compiler.cpp
@@ -13,8 +13,8 @@ Compiler::Compiler(std::shared_ptr<logging::Logger> logger)
 
 void Compiler::read_file(std::unique_ptr<std::istream> stream, std::string_view file_name)
 {
-	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(move(stream));
-	lexer::Lexer lexer(move(reader));
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(stream));
+	lexer::Lexer lexer(std::move(reader));
 
 	try
 	{
