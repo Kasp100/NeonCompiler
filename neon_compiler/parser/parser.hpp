c54ffcb5805ec06f9a9d12e91fa39e7c300c0b04
Kasper Declercq <kasper.declercq1@gmail.com>
2025-07-01 15:56:15 +0200

    Use span in parser.hpp

--- neon_compiler/parser/parser.hpp
+++ neon_compiler/parser/parser.hpp
@@ -1,15 +1,18 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-namespace compiler::ast
+#include <span>
+#include "../token.hpp"
+
+namespace neon_compiler::parser
 {
 
 class Parser
 {
 public:
-    explicit Parser();
+    explicit Parser(std::span<neon_compiler::Token> tokens);
 private:
-
+    std::span<neon_compiler::Token> tokens;
 };
 
 }
