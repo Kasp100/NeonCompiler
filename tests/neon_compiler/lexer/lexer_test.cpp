d77932ea5d2d35036aca327a5848db8f0252ee08
Kasper Declercq <kasper.declercq1@gmail.com>
2025-07-02 14:23:30 +0200

    Create string literal parsing test

--- tests/neon_compiler/lexer/lexer_test.cpp
+++ tests/neon_compiler/lexer/lexer_test.cpp
@@ -9,7 +9,9 @@
 
 using namespace neon_compiler::lexer;
 
-constexpr const char* TEST_KEYWORDS = "pkg interface mut:";
+constexpr const char
+		*TEST_KEYWORDS = "pkg interface mut:",
+		*TEST_STRING_LITERALS = "\"strings\",\"test\" \"ing\"";
 
 TEST_CASE("Keywords are parsed correctly")
 {
@@ -30,4 +32,27 @@ TEST_CASE("Keywords are parsed correctly")
 	CHECK(tokens[0].get_type() == neon_compiler::TokenType::PACKAGE);
 	CHECK(tokens[1].get_type() == neon_compiler::TokenType::TYPE_INTERFACE);
 	CHECK(tokens[2].get_type() == neon_compiler::TokenType::MUTABLE_REFERENCE);
+}
+
+TEST_CASE("String literals are parsed correctly")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_STRING_LITERALS);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	CHECK(lexer.get_errors().size() == 0);
+
+	std::span<const neon_compiler::Token> tokens = lexer.get_tokens();
+	CHECK(tokens.size() == 3);
+
+	CHECK(tokens[0].get_type() == neon_compiler::TokenType::LITERAL_STRING);
+	CHECK(tokens[0].get_lexeme().value() == "strings");
+	CHECK(tokens[1].get_type() == neon_compiler::TokenType::COMMA);
+	CHECK(tokens[2].get_type() == neon_compiler::TokenType::LITERAL_STRING);
+	CHECK(tokens[2].get_lexeme().value() == "testing");
 }
