e48009dcc3b79c137378a236a992f22f1a4edff2
Kasper Declercq <kasper.declercq1@gmail.com>
2025-07-04 23:28:38 +0200

    Add tests for number literal parsing

--- tests/neon_compiler/lexer/lexer_test.cpp
+++ tests/neon_compiler/lexer/lexer_test.cpp
@@ -14,7 +14,13 @@ constexpr const char
 		*TEST_LITERAL_STRING = "\"strings\",\"test\" \"ing\"",
 		*TEST_LITERAL_CHARACTER = "'c' '\\'' '\\n'",
 		*TEST_ILLEGAL_LITERAL_STRING = "\"a\\qb\nc",
-		*TEST_ILLEGAL_LITERAL_CHARACTER = "'ab'";
+		*TEST_ILLEGAL_LITERAL_CHARACTER = "'ab'",
+		*TEST_ILLEGAL_LITERAL_NUMBER_ILLEGAL_DIGITS_HEXADECIMAL = "0x012345689ABCDEFG",
+		*TEST_ILLEGAL_LITERAL_NUMBER_ILLEGAL_DIGITS_BINARY = "0b012",
+		*TEST_ILLEGAL_LITERAL_NUMBER_ILLEGAL_DIGITS_DECIMAL = "0123456789AG",
+		*TEST_ILLEGAL_LITERAL_NUMBER_PREFIX_WITHOUT_DIGITS_HEXADECIMAL = "0x",
+		*TEST_ILLEGAL_LITERAL_NUMBER_PREFIX_WITHOUT_DIGITS_BINARY = "0b",
+		*TEST_ILLEGAL_LITERAL_NUMBER_MULTIPLE_DECIMAL_POINTS = "70.1.7";
 
 TEST_CASE("Keywords are parsed correctly")
 {
@@ -119,4 +125,107 @@ TEST_CASE("Illegal character literals are disallowed")
 
 	CHECK(errors[0].get_message() == error_messages::CHARACTER_LITERAL_TOO_LONG);
 }
+
+TEST_CASE("Illegal hexadecimal digits aren't allowed")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_ILLEGAL_LITERAL_NUMBER_ILLEGAL_DIGITS_HEXADECIMAL);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	std::span<const neon_compiler::lexer::TokenisationError> errors = lexer.get_errors();
+	CHECK(errors.size() == 1);
+
+	CHECK(errors[0].get_message() == error_messages::ILLEGAL_DIGITS_IN_NUMBER_LITERAL);
+}
+
+TEST_CASE("Illegal binary digits aren't allowed")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_ILLEGAL_LITERAL_NUMBER_ILLEGAL_DIGITS_BINARY);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	std::span<const neon_compiler::lexer::TokenisationError> errors = lexer.get_errors();
+	CHECK(errors.size() == 1);
+
+	CHECK(errors[0].get_message() == error_messages::ILLEGAL_DIGITS_IN_NUMBER_LITERAL);
+}
+
+TEST_CASE("Illegal hexadecimal digits aren't allowed")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_ILLEGAL_LITERAL_NUMBER_ILLEGAL_DIGITS_DECIMAL);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	std::span<const neon_compiler::lexer::TokenisationError> errors = lexer.get_errors();
+	CHECK(errors.size() == 1);
+
+	CHECK(errors[0].get_message() == error_messages::ILLEGAL_DIGITS_IN_NUMBER_LITERAL);
+}
+
+TEST_CASE("Hexadecimal prefix without digits isn't allowed")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_ILLEGAL_LITERAL_NUMBER_PREFIX_WITHOUT_DIGITS_HEXADECIMAL);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	std::span<const neon_compiler::lexer::TokenisationError> errors = lexer.get_errors();
+	CHECK(errors.size() == 1);
+
+	CHECK(errors[0].get_message() == error_messages::NUMBER_BASE_PREFIX_WITHOUT_DIGITS);
+}
+
+TEST_CASE("Binary prefix without digits isn't allowed")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_ILLEGAL_LITERAL_NUMBER_PREFIX_WITHOUT_DIGITS_BINARY);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	std::span<const neon_compiler::lexer::TokenisationError> errors = lexer.get_errors();
+	CHECK(errors.size() == 1);
+
+	CHECK(errors[0].get_message() == error_messages::NUMBER_BASE_PREFIX_WITHOUT_DIGITS);
+}
+
+TEST_CASE("Multiple decimal points aren't allowed")
+{
+	// Arrange
+	std::unique_ptr<std::istringstream> iss = std::make_unique<std::istringstream>(TEST_ILLEGAL_LITERAL_NUMBER_MULTIPLE_DECIMAL_POINTS);
+	std::unique_ptr<reading::CharReader> reader = std::make_unique<reading::CharReader>(std::move(iss));
+
+	// Act
+	Lexer lexer{std::move(reader)};
+	lexer.run();
+
+	// Assert
+	std::span<const neon_compiler::lexer::TokenisationError> errors = lexer.get_errors();
+	CHECK(errors.size() == 1);
+
+	CHECK(errors[0].get_message() == error_messages::MULTIPLE_DECIMAL_POINTS_IN_NUMBER_LITERAL);
+}
+
 
