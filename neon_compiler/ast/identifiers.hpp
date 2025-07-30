ee1e7ddf77e2b080db5df0685927f3cdbb194cf2
Kasper Declercq <kasper.declercq1@gmail.com>
2025-07-30 23:35:29 +0200

    Redo identifiers

---
+++ neon_compiler/ast/identifiers.hpp
@@ -0,0 +1,19 @@
+#ifndef IDENTIFIERS_HPP
+#define IDENTIFIERS_HPP
+
+#include <vector>
+#include <string>
+
+struct Identifier
+{
+    /** Parts that together make an identifier.
+     * Identifiers for static objects (incl. packages and package members like types) may contain multiple parts.
+     * The symbol `::` separates the parts in source code.
+     * Example 1: `main::subpkg` in source code becomes "main" followed by "subpkg" in this vector.
+     * This is unambiguously static.
+     * Example 2: `my_variable` in source code becomes "my_variable" (single element) in this vector.
+     * It is later determined whether this is a static or non-static reference */
+    std::vector<std::string> parts;
+};
+
+#endif // IDENTIFIERS_HPP
