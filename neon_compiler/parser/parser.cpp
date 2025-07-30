d47c8f3d4e478e5748d3273795738e8c108785b8
Kasper Declercq <kasper.declercq1@gmail.com>
2025-07-30 23:47:35 +0200

    Create parsing code for identifiers

--- neon_compiler/parser/parser.cpp
+++ neon_compiler/parser/parser.cpp
@@ -13,27 +13,10 @@ void Parser::run()
 		errors.emplace_back(reader.peek(), error_messages::MISSING_PACKAGE_DECLARATION);
 	}
 
-	std::string package_name{};
-	while(!reader.consume_if_matches(TokenType::END_STATEMENT))
+	std::optional<Identifier> package_id = parse_identifier();
+	if(!package_id.has_value())
 	{
-		if(reader.end_of_file_reached())
-		{
-			errors.emplace_back(reader.peek(), error_messages::UNEXPECTED_END_OF_FILE);
-			return;
-		}
-		else if(reader.peek().get_type() == TokenType::IDENTIFIER)
-		{
-			std::string id_part{reader.consume().get_lexeme().value()};
-			package_name.append(id_part);
-		}
-		else if(reader.consume_if_matches(TokenType::STATIC_ACCESSOR))
-		{
-			package_name.append("::");
-		}
-		else
-		{
-			errors.emplace_back(reader.consume(), error_messages::UNEXPECTED_TOKEN_IN_PACKAGE_IDENTIFIER);
-		}
+		errors.emplace_back(reader.peek(), error_messages::MISSING_IDENTIFIER);
 	}
 }
 
@@ -49,4 +32,21 @@ void Parser::print_token(const Token& token)
 	{
 		logger->debug("Lexeme =\t" + std::string(token.get_lexeme().value()));
 	}
+}
+
+std::optional<Identifier> Parser::parse_identifier()
+{
+	std::vector<std::string> parts{};
+
+	do
+	{
+		if(reader.peek().get_type() != TokenType::IDENTIFIER)
+		{
+			return std::nullopt;
+		}
+		parts.emplace_back(reader.consume().get_lexeme().value());
+	}
+	while(reader.consume_if_matches(TokenType::STATIC_ACCESSOR));
+
+	return Identifier{parts};
 }
--- neon_compiler/parser/parser.hpp
+++ neon_compiler/parser/parser.hpp
@@ -3,25 +3,26 @@
 
 #include <span>
 #include <vector>
+#include <optional>
+#include <string>
 #include "../token.hpp"
 #include "../token_reader.hpp"
 #include "parsing_error.hpp"
 #include "../../logging/logger.hpp"
+#include "../ast/identifiers.hpp"
 
 namespace neon_compiler::parser
 {
 
 namespace error_messages
 {
-	constexpr std::string_view MISSING_PACKAGE_DECLARATION =
-			"Expecting a package declaration. Example: `pkg main::example;`";
 	constexpr std::string_view UNEXPECTED_END_OF_FILE =
 			"Unexpected end of file: a terminating token is missing. "
             "Statements are terminated with `;` and blocks with `}`.";
-	constexpr std::string_view UNEXPECTED_TOKEN_IN_PACKAGE_IDENTIFIER =
-			"Unexpected token in package identifier: alphanumeric names starting "
-            "with alphabetic characters should be used, with `::` for subpackages. "
-            "Example: `main::subpkg`";
+	constexpr std::string_view MISSING_PACKAGE_DECLARATION =
+			"Expected a package declaration. Example: `pkg main::example;`";
+	constexpr std::string_view MISSING_IDENTIFIER =
+			"Expected an identifier; keywords or other tokens cannot be used here.";
 }
 
 class Parser
@@ -35,6 +36,7 @@ private:
     std::vector<neon_compiler::parser::ParsingError> errors;
     std::shared_ptr<logging::Logger> logger;
     void print_token(const Token& token);
+    std::optional<Identifier> parse_identifier();
 };
 
 }
