e564056fda9788605b44d83ff8cdb53d5c0e5fbf
Kasper Declercq <kasper.declercq1@gmail.com>
2025-08-13 17:45:55 +0200

    Improve to_string

--- neon_compiler/ast/identifiers.hpp
+++ neon_compiler/ast/identifiers.hpp
@@ -6,29 +6,33 @@
 
 struct Identifier
 {
-    /** Parts that together make an identifier.
-     * Identifiers for static objects (incl. packages and package members like types) may contain multiple parts.
-     * The symbol `::` separates the parts in source code.
-     * Example 1: `main::subpkg` in source code becomes "main" followed by "subpkg" in this vector.
-     * This is unambiguously static.
-     * Example 2: `my_variable` in source code becomes "my_variable" (single element) in this vector.
-     * It is later determined whether this is a static or non-static reference */
-    std::vector<std::string> parts;
+	/** Parts that together make an identifier.
+	 * Identifiers for static objects (incl. packages and package members like types) may contain multiple parts.
+	 * The symbol `::` separates the parts in source code.
+	 * Example 1: `main::subpkg` in source code becomes "main" followed by "subpkg" in this vector.
+	 * This is unambiguously static.
+	 * Example 2: `my_variable` in source code becomes "my_variable" (single element) in this vector.
+	 * It is later determined whether this is a static or non-static reference */
+	std::vector<std::string> parts;
 
-    std::string to_string()
-    {
-        std::string s{};
-        bool first = true;
-        for(const std::string& part : parts)
-        {
-            if(!(first = false))
-            {
-                s += "::";
-            }
-            s += part;
-        }
-        return s;
-    }
+	std::string to_string()
+	{
+		std::string s{};
+		bool first = true;
+		for(const std::string& part : parts)
+		{
+			if(first)
+			{
+				first = false;
+			}
+			else
+			{
+				s += "::";
+			}
+			s += part;
+		}
+		return s;
+	}
 };
 
 #endif // IDENTIFIERS_HPP
