8515d05689546b556024b32a5763f927748222b5
Kasper Declercq <kasper.declercq1@gmail.com>
2026-09-11 12:41:12 +0200

    Make DeclarationAnalyser an ASTVisitor

--- neon_compiler/semantic_analyser/declaration_analyser.cpp
+++ neon_compiler/semantic_analyser/declaration_analyser.cpp
@@ -1,2 +1,174 @@
 #include "declaration_analyser.hpp"
 
+using namespace neon_compiler::semantic_analyser;
+using namespace neon_compiler::ast::nodes;
+
+DeclarationAnalyser::DeclarationAnalyser()
+{
+
+}
+
+void DeclarationAnalyser::visit(const Root& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const FileNode& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const TypeDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const VariableDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const ConstantDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const FieldDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const MethodDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const ReferenceType& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const CodeBlock& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const DiscardExpression& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const LocalDeclarationOrAssignment& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const UseStatement& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const AutoCall& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const Return& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const Assignment& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const Give& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const GiveAndAssign& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const ObjectFunctionCall& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const ObjectRead& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const FunctionCall& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const SimpleRead& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const OptionalEmpty& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const PackageFunctionDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const OperatorModule& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const OperatorDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const OperatorFunctionDeclaration& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const LiteralNumberExpression& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const LiteralStringExpression& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const LiteralBooleanExpression& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const OperatorCallExpression& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const CheckPresence& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const CheckAbsence& node)
+{
+
+}
+
+void DeclarationAnalyser::visit(const Fallback& node)
+{
+
+}
--- neon_compiler/semantic_analyser/declaration_analyser.hpp
+++ neon_compiler/semantic_analyser/declaration_analyser.hpp
@@ -1,6 +1,51 @@
 #ifndef DECLARATION_ANALYSER_HPP
 #define DECLARATION_ANALYSER_HPP
 
+#include "../ast/ast_visitor.hpp"
 
+namespace neon_compiler::semantic_analyser
+{
+
+class DeclarationAnalyser : neon_compiler::ast::ASTVisitor
+{
+public:
+    explicit DeclarationAnalyser();
+	void visit(const neon_compiler::ast::nodes::Root& node) override;
+	void visit(const neon_compiler::ast::nodes::FileNode& node) override;
+	void visit(const neon_compiler::ast::nodes::TypeDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::VariableDeclaration& node) override;
+    void visit(const neon_compiler::ast::nodes::ConstantDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::FieldDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::MethodDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::ReferenceType& node) override;
+	void visit(const neon_compiler::ast::nodes::CodeBlock& node) override;
+	void visit(const neon_compiler::ast::nodes::DiscardExpression& node) override;
+	void visit(const neon_compiler::ast::nodes::LocalDeclarationOrAssignment& node) override;
+	void visit(const neon_compiler::ast::nodes::UseStatement& node) override;
+	void visit(const neon_compiler::ast::nodes::AutoCall& node) override;
+	void visit(const neon_compiler::ast::nodes::Return& node) override;
+	void visit(const neon_compiler::ast::nodes::Assignment& node) override;
+	void visit(const neon_compiler::ast::nodes::Give& node) override;
+	void visit(const neon_compiler::ast::nodes::GiveAndAssign& node) override;
+	void visit(const neon_compiler::ast::nodes::ObjectFunctionCall& node) override;
+	void visit(const neon_compiler::ast::nodes::ObjectRead& node) override;
+	void visit(const neon_compiler::ast::nodes::FunctionCall& node) override;
+	void visit(const neon_compiler::ast::nodes::SimpleRead& node) override;
+	void visit(const neon_compiler::ast::nodes::OptionalEmpty& node) override;
+	void visit(const neon_compiler::ast::nodes::PackageFunctionDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::OperatorModule& node) override;
+	void visit(const neon_compiler::ast::nodes::OperatorDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::OperatorFunctionDeclaration& node) override;
+	void visit(const neon_compiler::ast::nodes::LiteralNumberExpression& node) override;
+	void visit(const neon_compiler::ast::nodes::LiteralStringExpression& node) override;
+	void visit(const neon_compiler::ast::nodes::LiteralBooleanExpression& node) override;
+	void visit(const neon_compiler::ast::nodes::OperatorCallExpression& node) override;
+	void visit(const neon_compiler::ast::nodes::CheckPresence& node) override;
+	void visit(const neon_compiler::ast::nodes::CheckAbsence& node) override;
+	void visit(const neon_compiler::ast::nodes::Fallback& node) override;
+
+};
+
+}
 
 #endif // DECLARATION_ANALYSER_HPP
