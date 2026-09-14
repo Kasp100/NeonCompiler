bd9664d09c77b28e11b12d7cb6c07948c8d72014
Kasper Declercq <kasper.declercq1@gmail.com>
2026-09-14 19:06:29 +0200

    Create symbol types

--- neon_compiler/semantic_analyser/symbols.hpp
+++ neon_compiler/semantic_analyser/symbols.hpp
@@ -1,9 +1,52 @@
+#include <cstdint>
+#include <string>
+#include "../ast/nodes/nodes.hpp"
+
+namespace neon_compiler::semantic_analyser
+{
+
+using MutabilityMode = neon_compiler::ast::nodes::MutabilityMode;
+
+struct VariableID
+{
+    uint32_t value;
+};
+
+struct FunctionID
+{
+    uint32_t value;
+};
+
+struct TypeID
+{
+    uint32_t value;
+};
+
 struct Symbol
 {
+    std::string name;
+
 	virtual ~Symbol() = default;
 };
 
-struct FunctionSymbol
+struct TypeSymbol : Symbol
 {
-	
+    bool supports_mutable_state;
 };
+
+struct VariableSymbol : Symbol
+{
+    bool mutating_permission;
+    MutabilityMode mutability_mode;
+    TypeID type;
+};
+
+struct FunctionSymbol : Symbol
+{
+    bool return_mutating_permission;
+    MutabilityMode return_mutability_mode;
+	TypeID return_type;
+    std::vector<VariableID> parameters;
+};
+
+}
